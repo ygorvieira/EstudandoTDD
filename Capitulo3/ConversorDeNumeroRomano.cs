a7a7d0a20c20645fa48965590f301a64c484edc3
Ygor Vieira <ygorus@gmail.com>
2018-06-29 18:44:11 -0300

    Testando algarismos romanos

---
+++ Capitulo3/ConversorDeNumeroRomano.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections.Generic;
+
+namespace Capitulo3
+{
+    internal class ConversorDeNumeroRomano
+    {
+        private static Dictionary<string, int> tabela = new Dictionary<string, int>()
+        {
+            { "I", 1 },
+            { "V", 5 },
+            { "X", 10 },
+            { "L", 50 },
+            { "C", 100 },
+            { "D", 500 },
+            { "M", 1000 }
+        };
+
+        public int Converte(string numeroRomano)
+        {
+            int acumulador = 0;
+            int ultimoVizinhoDaDireita = 0;
+
+            for (int i = numeroRomano.Length -1; i >= 0; i--)
+            {
+                int atual = tabela[numeroRomano[i].ToString()];
+
+                int multiplicador = 1;
+
+                if (atual < ultimoVizinhoDaDireita)
+                    multiplicador = -1;
+
+                acumulador += atual * multiplicador;
+
+                ultimoVizinhoDaDireita = atual;
+            }
+            return acumulador;
+        }
+    }
+}
---
+++ Capitulo3/NumerosRomanosTest.cs
@@ -0,0 +1,48 @@
+﻿using NUnit.Framework;
+
+namespace Capitulo3
+{
+    [TestFixture]
+    public class NumerosRomanosTest
+    {
+        [Test]
+        public void DeveEntenderOSimboloI()
+        {
+            ConversorDeNumeroRomano romano = new ConversorDeNumeroRomano();
+            int numero = romano.Converte("I");
+            Assert.AreEqual(1, numero);
+        }
+
+        [Test]
+        public void DeveEntenderOSimboloV()
+        {
+            ConversorDeNumeroRomano romano = new ConversorDeNumeroRomano();
+            int numero = romano.Converte("V");
+            Assert.AreEqual(5, numero);
+        }
+
+        [Test]
+        public void DeveEntenderDoisSimbolosComoII()
+        {
+            ConversorDeNumeroRomano romano = new ConversorDeNumeroRomano();
+            int numero = romano.Converte("II");
+            Assert.AreEqual(2, numero);
+        }
+
+        [Test]
+        public void DeveEntenderQuatroSimbolosDoisADoisComoXXII()
+        {
+            ConversorDeNumeroRomano romano = new ConversorDeNumeroRomano();
+            int numero = romano.Converte("XXII");
+            Assert.AreEqual(22, numero);
+        }
+
+        [Test]
+        public void DeveEntenderSimbolosDiferentesComoIX()
+        {
+            ConversorDeNumeroRomano romano = new ConversorDeNumeroRomano();
+            int numero = romano.Converte("IX");
+            Assert.AreEqual(9, numero);
+        }
+    }
+}
